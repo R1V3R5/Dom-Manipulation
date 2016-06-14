d3a1b7dc9ccec210230656f36da3085479701592
Joshua Rivers <joshua.rivers@gmail.com>
2016-06-14 15:22:49 -0400

    Have calcluator mocked up and showing, no js functionality yet

---
+++ hard-mode/main.js
@@ -0,0 +1,22 @@
+// var calc = document.querySelector(".calc-buttons");
+// calc.addEventListener('click', function (event) {
+//   console.log(event)
+// })
+
+
+var one= document.querySelector(".one");
+var two= document.querySelector(".two");
+var three= document.querySelector(".three");
+var four= document.querySelector(".four");
+var five= document.querySelector(".five");
+var six= document.querySelector(".six");
+var seven= document.querySelector(".seven");
+var eight= document.querySelector(".eight");
+var nine= document.querySelector(".nine");
+var add= document.querySelector(".add");
+var subtract= document.querySelector(".subtract");
+var multiply= document.querySelector(".multiply");
+var divide= document.querySelector(".divide");
+var equal= document.querySelector(".equal");
+var period= document.querySelector(".period");
+var clear= document.querySelector(".clear");
--- normal-mode/main.js
+++ normal-mode/main.js
@@ -4,7 +4,5 @@ calculate.addEventListener('click', function (event) {
   var input1 = document.querySelector("#value-one");
   var input2 = document.querySelector("#value-two");
   var answer = document.querySelector("#answer");
-  input1.textContent = input1.value;
-  input2.textContent = input2.value;
   answer.textContent = (Number(input1.value) + Number(input2.value));
 });
